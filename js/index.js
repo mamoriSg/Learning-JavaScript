75dfa6f756f9d8673b89d4dac46b7e951501749d
Oleksandr <grozdev.i14@gmail.com>
2024-08-09 18:33:25 +0300

    4 lesson

--- js/index.js
+++ js/index.js
@@ -1,31 +1,11 @@
-// var number = 15;
-// var isHasCar = true;
+var data = [4, 5, 2, 52, "Word", true];
+data[0] = false;
+console.log(data.length);
 
-// if (number > 15 || isHasCar) {
-//     console.log("Yes");
-//     console.log("Yes. his car");
-// }
-// else if (number == 15) {
-//     console.log("num is 15");
-// }
+var matrix = [[5, 6],
+    ["World", false],
+    [true, 5.9, 0]
+];
 
-// else {
-//     console.log("Else")
-// }
-
-var word = "3";
-
-switch (word) {
-    case "new":
-        console.log("var is new");
-        break;
-    case 5:
-        console.log("var is 5");
-        break;
-    case "some":
-        console.log("var is some");
-        break;
-    default:
-        console.log("vai is unknown");
-        break;
-}
+matrix[0][0] = 60;
+console.log(matrix[2][1]);
