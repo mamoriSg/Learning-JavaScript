36c01a6b9717bee95192e0adce628693d7dce965
Oleksandr <grozdev.i14@gmail.com>
2024-08-12 21:21:22 +0300

    8 lesson

--- js/index.js
+++ js/index.js
@@ -1,18 +1,8 @@
-var some = 5;
-
-function arrSum(arr) {
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++)
-        sum += arr[i];
-
-    console.log(some);
-    return sum;
+function btnClick() {
+    console.log("message");
 }
-var arr1 = [5, 3, 2, 2];
-var res1 = arrSum(arr1); 
 
-var arr2 = [5, 3, 2, 2, 10];
-var res2 = arrSum(arr2);
-
-if (res1 > res2) console.log(res1);
-else console.log(res2);
+function btnOnceClick(el) {
+    el.name = "new value"
+    alert(el.name); 
+}
