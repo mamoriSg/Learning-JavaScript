4606d9a2b3832502c3f7bf48a29f4f3cc866efdc
Oleksandr <grozdev.i14@gmail.com>
2024-08-26 21:00:41 +0300

    9 lesson

--- js/index.js
+++ js/index.js
@@ -2,7 +2,20 @@ function btnClick() {
     console.log("message");
 }
 
+var counter = 0; 
 function btnOnceClick(el) {
-    el.name = "new value"
-    alert(el.name); 
+    counter++;
+    el.innerHTMl = "count: " + counter;
+    // el.name = "new value"
+    // alert(el.name); 
+}
+
+function userInput(el) {
+    if (el.value == "font");
+    el.style.fontSize = "20px"; 
+    
+    else if (el.value == "color")
+        el.style.color = "red";
+    else if (el.value == "color")
+        el.style.backgroundColor = "black";
 }
