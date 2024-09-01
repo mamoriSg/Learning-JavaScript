7aeeee6c847e92f2fb98835e2767e97b3d496716
Oleksandr <grozdev.i14@gmail.com>
2024-09-01 09:56:25 +0300

    11 lesson

--- js/index.js
+++ js/index.js
@@ -1,25 +1,20 @@
-function checkForm() {
-    var username = el.username.value;
-    var email = el.email.value;
-    var pass = el.password.value;
-    var gender = el.gender.value;
+var id = setInterval(my_func, 1000);
 
-    var error = '';
-    if (username.length < 2)
-        error = "Ім'я занадто мале";
-    else if (email.length < 2)
-        error = "Email занадто малий";
-    else if (!email.includes("@"))
-        error = "Email не має @";
-    else if (pass.length < 2)
-        error = "Pass занадто малий";
-    else if (gender == null || gender == '')
-        error = "Gender error ";
+var counter = 0;
+function my_func() {
+    document.getElementById("timer").innerHTML = "Counter: " + counter;
+    counter++;
+}
 
-    if (error != "") {
-        document.getElementById("error").innerText = error;
-        return false;
-    }
-    
-    return false;
-}   
+function stopInterval() {
+    // clearInterval(id);
+    clearTimeout(timerid);
+}
+
+setInterval(function () {
+    console.log('sec...');
+}, 2000);
+
+var timerid = setTimeout(function () {
+    alert("Message");
+}, 5000);
